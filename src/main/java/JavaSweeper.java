edfeab5f8e1fc23e74863a1f8d6cee61e98f3fc6
Veronika <koraf9712@gmail.com>
2022-09-28 22:34:47 +0200

    added images and an anonymous class for adding images to the app window

--- src/main/java/JavaSweeper.java
+++ src/main/java/JavaSweeper.java
@@ -23,10 +23,23 @@ public class JavaSweeper extends JFrame {
     }
 
     private void initPanel() {
-        panel = new JPanel();
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(getImage("bomb"), 0, 0, this);
+                //Anonymous class for creating and adding images
+            }
+        };
         panel.setPreferredSize(new Dimension(
                 COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE)); //setting size of an app-window
         add(panel); //JFrame method
+    }
+
+    private Image getImage(String name) {
+        String fileName = "img/" + name.toLowerCase() + ".png";
+        ImageIcon icon = new ImageIcon(getClass().getResource(fileName));
+        return icon.getImage();
     }
 
     public static void main(String[] args) {
